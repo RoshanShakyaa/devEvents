d439b13a7a29101472da770d58fc2251dba2f933
Roshan Shakya <roshan.shakyaa2015@gmail.com>
2025-11-18 17:11:06 +0545

    implement api, server actions and caching and event details page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import { events } from "@/lib/constants";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
+import { Suspense } from "react";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Home = () => {
   return (
     <>
@@ -16,14 +19,9 @@ const Home = () => {
         <ExploreBtn />
         <div className="mt-20 space-y-7">
           <h3>Featured Events</h3>
-
-          <ul className="events list-none">
-            {events.map((event) => (
-              <li key={event.title}>
-                <EventCard {...event} />
-              </li>
-            ))}
-          </ul>
+          <Suspense fallback={<p>Loading...</p>}>
+            <PublicEvents />
+          </Suspense>
         </div>
       </section>
     </>
@@ -31,3 +29,22 @@ const Home = () => {
 };
 
 export default Home;
+
+const PublicEvents = async () => {
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
+  return (
+    <ul className="events list-none">
+      {events &&
+        events.length > 0 &&
+        events.map((event: IEvent) => (
+          <li key={event.title}>
+            <EventCard {...event} />
+          </li>
+        ))}
+    </ul>
+  );
+};
